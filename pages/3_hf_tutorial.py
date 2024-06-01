a6698f08b3b6158f5ad3637eed21428fa3a6bd79
Dev Makwana <devmakwana7704@gmail.com>
2024-06-01 11:46:34 +0530

    update 3.7

--- pages/3_hf_tutorial.py
+++ pages/3_hf_tutorial.py
@@ -4,7 +4,6 @@ import numpy as np
 from transformers import pipeline
 
 st.title('3 - *HuggingFace* :blue[Tutorial]')
-st.divider()
 
 ######################################################
 st.subheader('Pipe1 :- Sentiment Analysis',divider='orange')
@@ -12,15 +11,11 @@ st.subheader('Pipe1 :- Sentiment Analysis',divider='orange')
 if st.checkbox(label='Show Pipe1'):
     classifier = pipeline('sentiment-analysis')
 
-    sentence = "I've been waiting for a huggingface course my whole life."
-    res = classifier(sentence)
-
-    st.write("Given Sentence is :",sentence)
-    st.write(res)
     x = st.text_input(label='Enter text', value="I've been waiting for a huggingface course my whoole life.")
     res = classifier(x)
     st.markdown(body=f"*Prediction*: :green-background[{res[0]['label']}]")
     st.markdown(f"*Score*: :green-background[{res[0]['score']}]")
+    st.write(res)
 
 ######################################################
 st.subheader('Pipe2 :- Text Generation',divider='orange')
@@ -32,14 +27,11 @@ if st.checkbox(label='Show Pipe2'):
         sentence,
         max_length = 30,
     )
-    st.write(res2)
-    st.write("Prompt is: ", sentence)
-    st.write("Generated text is: ")
-    st.write(res2[0]['generated_text'])
     x = st.text_input(label='Enter text', value="In this course we'll teach you how to")
     res2 = generator(x,max_length=50)
     st.write("Generated text is:")
-    st.markdown(res2[0]['generated_text'])
+    st.markdown(f":green[{res2[0]['generated_text']}]")
+    st.write(res2)
 
 
 ######################################################
@@ -51,16 +43,9 @@ if st.checkbox(label='Show Pipe3'):
         model = 'distilbert/distilbert-base-uncased-finetuned-sst-2-english',
         framework='pt'
     )
-    sentence = "This is a course about python list comprehension"
-    res3 = clf2(
-        sentence,
-        candidate_labels = ['education', 'politics', 'business']
-    )
-    st.write("Text is: ", sentence)
-    st.write(res3)
     x = st.text_input(label='Enter text', value="This is a course about python list comprehension")
     res3 = clf2(
         x,
         candidate_labels = ['education', 'politics', 'business']
     )
-    st.markdown(res3)
+    st.write(res3)
