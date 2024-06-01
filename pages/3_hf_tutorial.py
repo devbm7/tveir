4cc8addd8db157b3a1af962145829db713e44c8a
Dev Makwana <devmakwana7704@gmail.com>
2024-06-01 10:53:28 +0530

    update 3

--- pages/3_hf_tutorial.py
+++ pages/3_hf_tutorial.py
@@ -4,3 +4,49 @@ import numpy as np
 from transformers import pipeline
 
 st.title('3 - *HuggingFace* :blue[Tutorial]')
+
+######################################################
+st.subheader('Pipe1',divider='orange')
+
+if st.checkbox(label='Show Pipe1'):
+    classifier = pipeline('sentiment-analysis')
+
+    sentence = "I've been waiting for a huggingface course my whole life."
+    res = classifier(sentence)
+
+    st.write(res)
+    st.write("Given Sentence is :",sentence)
+    st.write("Prediction for this: ", res[0]['label'])
+    st.write("Score for this: ", res[0]['score'])
+
+######################################################
+st.subheader('Pipe2',divider='orange')
+
+if st.checkbox(label='Show Pipe2'):
+    generator = pipeline('text-generation', model='distilgpt2')
+    sentence = "In this course we'll teach you how to"
+    res2 = generator(
+        sentence,
+        max_length = 30,
+    )
+    st.write("Prompt is: ", sentence)
+    st.write(res2)
+    st.write("Generated text is: ")
+    st.write(res2[0]['generated_text'])
+
+
+######################################################
+st.subheader('Pipe3', divider='orange')
+
+if st.checkbox(label='Show Pipe3'):
+    clf2 = pipeline(
+        task='zero-shot-classification',
+        framework='pt'
+    )
+    sentence = "This is a course about python list comprehension"
+    res3 = clf2(
+        sentence,
+        candidate_labels = ['education', 'politics', 'business']
+    )
+    st.write("Text is: ", sentence)
+    st.write(res3)
