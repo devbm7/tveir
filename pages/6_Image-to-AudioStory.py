e0ec3fda063a0bd117d42848dc9d05801145a26b
Dev Makwana <devmakwana7704@gmail.com>
2024-06-02 11:39:02 +0530

    02062024 0.6

--- pages/6_Image-to-AudioStory.py
+++ pages/6_Image-to-AudioStory.py
@@ -1,32 +1,22 @@
 import streamlit as st
 import requests
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
-from transformers import AutoModel
-from transformers import pipeline
-import torch
-from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from transformers import BlipProcessor, BlipForConditionalGeneration, SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
 from datasets import load_dataset
 import torch
 import soundfile as sf
-
+import io
 
 st.header('5 - Image-To-AudioStory', divider='violet')
 
 # Ensure the model and processor are loaded outside the function to avoid reloading them on every run
 @st.cache(allow_output_mutation=True)
-def load_model():
+def load_image_captioning_model():
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
     return processor, model
 
-processor, model = load_model()
-
-img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg'
-raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
-
-# Define a function for generating captions
-def generate_caption(image, text=None):
+def generate_caption(processor, model, image, text=None):
     if text:
         inputs = processor(image, text, return_tensors="pt")
     else:
@@ -36,39 +26,40 @@ def generate_caption(image, text=None):
     caption = processor.decode(out[0], skip_special_tokens=True)
     return caption
 
+processor, model = load_image_captioning_model()
+
+img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg'
+raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
 # Conditional image captioning
 text = "a photography of"
-conditional_caption = generate_caption(raw_image, text)
+conditional_caption = generate_caption(processor, model, raw_image, text)
 st.write(f"Conditional caption: {conditional_caption}")
 
 # Unconditional image captioning
-unconditional_caption = generate_caption(raw_image)
+unconditional_caption = generate_caption(processor, model, raw_image)
 st.write(f"Unconditional caption: {unconditional_caption}")
 
-## llm - story gen
+## Text-to-Speech
 
-## text2speech
+# Load T5-based Text-to-Speech models
+processor_tts = SpeechT5Processor.from_pretrained("microsoft/speecht5-tts")
+model_tts = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5-tts")
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5-hifigan")
 
-processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
-vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+# Prepare inputs for Text-to-Speech
+inputs_tts = processor_tts(text="Hello, my dog is cute.", return_tensors="pt")
 
-inputs = processor(text="Hello, my dog is cute.", return_tensors="pt")
-
-# load xvector containing speaker's voice characteristics from a dataset
+# Load x-vector containing speaker's voice characteristics from a dataset
 embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
 speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 
-speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+# Generate speech
+speech = model_tts.generate_speech(inputs_tts["input_ids"], speaker_embeddings, vocoder=vocoder)
 
-sf.write("speech.wav", speech.numpy(), samplerate=16000)
+# Save speech to a file
+file_obj = io.BytesIO()
+sf.write(file_obj, speech.numpy(), samplerate=22050, format='wav')
 
-st.audio("speech.wav", format='audio/wav')
-# audio_pipeline = pipeline(
-#     task = 'text-to-speech',
-# )
-
-# audio_output = audio_pipeline(unconditional_caption)[0]['audio']
-# st.audio(audio_output, format='audio/wav')
-# audio_output = audio_pipeline(unconditional_caption)
-# st.audio(data=audio_output)
+# Display the audio file
+st.audio(file_obj, format='audio/wav')
